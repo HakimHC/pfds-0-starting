13c6a57e66bb2fd6a1a38f0187ed3f887bd6c46d
HakimHC <ahmedh90111@gmail.com>
2023-10-28 16:41:59 +0200

    ex06 program finished

---
+++ ex06/filterstring.py
@@ -0,0 +1,38 @@
+import sys
+from ft_filter import ft_filter
+
+def validate(n):
+    try:
+        int(n)
+    except ValueError:
+        raise AssertionError("Cannot convert to integer")
+
+def main():
+    """ Main function """
+    argv = sys.argv
+    argc = len(argv)
+
+    try:
+        assert argc == 3, "Invalid Arguments";
+    except AssertionError as e:
+        print(f"{ type(e).__name__ }: { str(e) }")
+        return
+
+    try:
+        validate(argv[2])
+    except AssertionError as e:
+        print(f"{ type(e).__name__ }: { str(e) }")
+        return
+
+    min_len = int(argv[2])
+
+    iter = argv[1].split(" ")
+    res = ft_filter(lambda x: len(x) >= min_len, iter)
+    print(res)
+
+
+
+if __name__ == "__main__":
+    # print(ft_filter.__doc__)
+    # print(filter.__doc__)
+    main()
---
+++ ex06/ft_filter.py
@@ -0,0 +1,10 @@
+def ft_filter(function, iterable):
+    """ft_filter(function or None, iterable) --> filter object
+
+Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true."""
+    if function:
+        res = [item for item in iterable if function(item)]
+    else:
+        res = [item for item in iterable if item]
+    return res
