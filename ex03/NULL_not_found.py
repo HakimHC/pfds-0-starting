ddc8f57b6a5d9f5e0378e8f022fd74e46ce60ace
HakimHC <ahmedh90111@gmail.com>
2023-10-28 15:19:06 +0200

    ex03 seems finished

---
+++ ex03/NULL_not_found.py
@@ -0,0 +1,30 @@
+EXIT_SUCCESS = 0
+EXIT_FAILURE = 1
+
+from types import NoneType
+from typing import Any
+import math
+
+def NULL_not_found(object: Any) -> int:
+    ret = EXIT_FAILURE
+    res = type(object)
+
+    if res == float and math.isnan(object):
+        print(f"nan { res }")
+        return EXIT_SUCCESS
+
+    tab = {
+            float: "NaN",
+            int: 0,
+            str: "",
+            bool: False,
+            NoneType: "None"
+    }
+
+    to_print = "Type not found"
+    if not object:
+        to_print = f"{ tab[res] } { res }"
+        ret = EXIT_SUCCESS
+
+    print(to_print)
+    return ret
