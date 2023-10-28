1416d5b271ec8b4131403c4d37677368ea454e32
HakimHC <ahmedh90111@gmail.com>
2023-10-28 14:24:36 +0200

    ex00 finished

---
+++ ex00/Hello.py
@@ -0,0 +1,9 @@
+ft_list = [ "Hello", "World" ]
+ft_tuple = ( "Hello", "Spain" )
+ft_set = { "Hello", "Madrid" }
+ft_dict = { "Hello": "42Madrid" }
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
