13a1ae78b68d56deedcb352c24521962202b4c96
HakimHC <ahmedh90111@gmail.com>
2023-10-28 14:38:30 +0200

    Added main check to ex01

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,12 +1,13 @@
 import time
 from datetime import datetime
 
-seconds_since_epoch = time.time()
+def main():
+    seconds_since_epoch = time.time()
+    scientific_notation = f"{seconds_since_epoch:.4e}"
+    current_date = datetime.fromtimestamp(seconds_since_epoch).strftime('%b %d %Y')
+    
+    print(f"Seconds since January 1, 1970: {seconds_since_epoch:.4f} or {scientific_notation} in scientific notation")
+    print(current_date)
 
-scientific_notation = f"{seconds_since_epoch:.4e}"
-
-print(f"Seconds since January 1, 1970: {seconds_since_epoch:.4f} or {scientific_notation} in scientific notation")
-
-current_date = datetime.fromtimestamp(seconds_since_epoch).strftime('%b %d %Y')
-
-print(current_date)
+if __name__ == "__main__":
+    main()
