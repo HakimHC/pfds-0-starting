b14dcec44177fc77f83065ae3ecf7a32b98af67a
HakimHC <ahmedh90111@gmail.com>
2023-10-28 16:53:20 +0200

    ex07 finished

---
+++ ex07/sos.py
@@ -0,0 +1,91 @@
+import sys
+
+NESTED_MORSE = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    " ": "/",
+}
+
+def main():
+    """ Main function """
+    argv = sys.argv
+    argc = len(argv)
+
+    try:
+        assert argc == 2, "Invalid Arguments";
+    except AssertionError as e:
+        print(f"{ type(e).__name__ }: { str(e) }")
+        return
+    
+    try:
+        if any(c not in NESTED_MORSE for c in argv[1]):
+            raise AssertionError("arguments are bad")
+    except AssertionError as e:
+        print(f"{ type(e).__name__ }: { str(e) }")
+        return
+
+    res = " ".join([ NESTED_MORSE[c] for c in argv[1] ])
+    print(res)
+    
+if __name__ == "__main__":
+    main()
